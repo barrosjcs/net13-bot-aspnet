ebc2287402335e7d6a3f5cc4180e6473f21b4a9f
barrosjcs <je_caetano@hotmail.com>
2018-09-05 22:38:11 -0300

    Teste com mongodb

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace SimpleBot
 {
@@ -9,12 +11,53 @@ namespace SimpleBot
     {
         public static string Reply(Message message)
         {
-            return $"{message.User} disse '{message.Text}'";
+            var client = new MongoClient("mongodb://localhost:27017");
+            var collection = new List<BsonDocument>();
+
+            //for (int i = 0; i < 5000; i++)
+            //{
+                var doc = new BsonDocument()
+                {
+                    { "id", message.Id },
+                    { "texto", message.Text },
+                    { "campo3",
+                        new BsonDocument {
+                            { "inner3", 2 }
+                        }
+                    }
+                    //,{ "contador", i }
+                };
+
+            //    collection.Add(doc);
+            //}
+
+            var db = client.GetDatabase("db01");
+            var col = db.GetCollection<BsonDocument>("tabela01");
+            //col.InsertOne(doc);
+            //col.InsertMany(collection);
+
+            UserProfile user = GetProfile(message.Id);
+
+            return $"{message.User} disse '{message.Text}' \n Id {user.Id}, teve {user.Visitas} visitas.";
+            
         }
 
         public static UserProfile GetProfile(string id)
         {
-            return null;
+            var client = new MongoClient("mongodb://localhost:27017");
+            var db = client.GetDatabase("db01");
+            var col = db.GetCollection<BsonDocument>("tabela01");
+
+            var filtro = Builders<BsonDocument>.Filter.Eq("id", id);
+            var res = col.Find(filtro).ToList();
+
+            UserProfile user = new UserProfile()
+            {
+                Id = id,
+                Visitas = res.Count()
+            };
+
+            return user;
         }
 
         public static void SetProfile(string id, UserProfile profile)
